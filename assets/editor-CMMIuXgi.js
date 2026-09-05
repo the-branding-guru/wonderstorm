73dd6c46053d9d8f7596408f854abd85801ef7e4
Jack Polo <jackpolo69@gmail.com>
2026-09-05 11:18:12 -0700

    deploy: wonderstorm 2026-09-05 11:18 from a236c0e2a

--- assets/editor-CMMIuXgi.js
+++ assets/editor-CMMIuXgi.js
@@ -1,4 +1,4 @@
-import{n as u,c as m,h as w}from"./index-CgHqnQXE.js";const b="living-copy:token";function A(c){const o=new Map,y=new WeakMap,i=document.createElement("div");i.id="living-copy-bar",i.setAttribute("data-living-copy-ignore",""),i.innerHTML=`
+import{n as u,c as m,h as w}from"./index-ClJQtL_y.js";const b="living-copy:token";function A(c){const o=new Map,y=new WeakMap,i=document.createElement("div");i.id="living-copy-bar",i.setAttribute("data-living-copy-ignore",""),i.innerHTML=`
     <style>
       #living-copy-bar {
         position: fixed; left: 50%; transform: translateX(-50%); bottom: 20px;
